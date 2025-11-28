2e7b7a711bcef5bfc8fd2bac034321a91e32d8d7
pramod ade <prmdextracthere@gmail.com>
2025-11-28 19:18:12 +0530

    test 7

--- src/components/App.js
+++ src/components/App.js
@@ -6,18 +6,9 @@ import Tab from "./Tab";
 const App = () => {
 
   const tab = [
-    {
-      titles:"Tab 1",
-     contents:"This is the content for Tab 1"
-    },
-    {
-      titles:"Tab 2",
-     contents:"This is the content for Tab 2"
-    },
-    {
-      titles:"Tab 3",
-     contents:"This is the content for Tab 3"
-    },
+  { titles:"Tab 1", contents:"This is the content for Tab 1." },
+  { titles:"Tab 2", contents:"This is the content for Tab 2." },
+  { titles:"Tab 3", contents:"This is the content for Tab 3." }
 ]
   return (
     <div> 
