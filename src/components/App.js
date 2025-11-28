e7eb3ce098d8d497bd192b3caea0791c65819219
pramod ade <prmdextracthere@gmail.com>
2025-11-28 18:15:48 +0530

    test 5

--- src/components/App.js
+++ src/components/App.js
@@ -7,16 +7,16 @@ const App = () => {
 
   const tab = [
     {
-      name:"Tab 1",
-     content:"This is the content for Tab 1"
+      titles:"Tab 1",
+     contents:"This is the content for Tab 1"
     },
     {
-      name:"Tab 2",
-     content:"This is the content for Tab 2"
+      titles:"Tab 2",
+     contents:"This is the content for Tab 2"
     },
     {
-      name:"Tab 3",
-     content:"This is the content for Tab 3"
+      titles:"Tab 3",
+     contents:"This is the content for Tab 3"
     },
 ]
   return (
--- src/components/Tab.js
+++ src/components/Tab.js
@@ -2,15 +2,16 @@ import React, { useState } from 'react'
 
 const Tab = ({tab}) => {
   const [contentName, setContentName] = useState("")
+  console.log(tab);
 
-  function handleClick(content){
-    setContentName(content)
+  function handleClick(contents){
+    setContentName(contents)
   }
   return (
     <div>
       {tab.map((item,index) => {
         return (<ul key={index}>
-          <li onClick={()=>handleClick(item.content)}>{item.name}</li>
+          <li onClick={()=>handleClick(item.contents)}>{item.titles}</li>
         </ul>)})}
       
       <p>{contentName}</p>
